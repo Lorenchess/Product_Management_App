1076f74ecc9e300cb3a032f43081400c1251ccdd
Ramon Lorente <ramon_lorente@yahoo.com>
2022-03-08 18:33:00 -0500

    First commit Lesson 6

---
+++ src/com/chess4math/data/Product.java
@@ -0,0 +1,100 @@
+package com.chess4math.data;
+
+import java.math.BigDecimal;
+
+import static com.chess4math.data.Rating.*;
+import static java.math.RoundingMode.HALF_UP;
+import java.time.LocalDate;
+import java.util.Objects;
+
+
+/**
+ *
+ * @author lorenchess
+ */
+public abstract class Product {
+
+    private int id;
+    private String name;
+    private BigDecimal price;
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+    private Rating rating;
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
+
+    public Product() {
+        this(0, "no name", BigDecimal.ZERO);
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public abstract Product applyRating(Rating newRating);
+//    {
+//        return new Product(id, name, price, newRating);
+//    }
+
+    /**
+     * Get the value of the best before date for the product
+     * @return the value of bestBefore
+     */
+
+    public LocalDate getBestBefore() {
+        return LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return id + ", " + name + ", " + price + ", "
+                + getDiscount() + ", " + rating.getStars()+" "+getBestBefore();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 83 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof Product) {
+            final Product other = (Product) obj;
+            return this.id == other.id && Objects.equals(this.name, other.name);
+        }
+        return false;
+    }
+
+
+}
+
